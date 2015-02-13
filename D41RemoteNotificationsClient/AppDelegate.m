11fe1ce2be41599e8d0b581393df9bdf4813d7c2
Rommel Rico <rommeltj@gmail.com>
2015-02-12 19:22:56 -0800

    Placeholders.

--- D41RemoteNotificationsClient/AppDelegate.m
+++ D41RemoteNotificationsClient/AppDelegate.m
@@ -26,6 +26,69 @@
     return YES;
 }
 
+-(void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+    //Determine if the user has allowed notifications
+    if (notificationSettings.types) {
+        //Now register for remote notifications.
+        [application registerForRemoteNotifications];
+    }
+}
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"deviceToken: %@", deviceToken);
+    
+    //Option 1: Try JSON.
+    //Put token into dictionary, then convert dictionary into JSON.
+    NSDictionary *d = @{@"deviceToken": deviceToken,
+                        @"username": @"johndoe" };
+    //Can this be converted to JSON?
+    if ([NSJSONSerialization isValidJSONObject:d]) {
+        //This can be converted to JSON
+        NSError *myError = nil;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:d options:0 error:&myError];
+        if (myError) {
+            NSLog(@"NSJSONSerialization error: %@", myError);
+            return;
+        }
+        //TODO: Send JSON data.
+    } else {
+        NSLog(@"Cannot be converted to JSON using NSJSONSerialization");
+    }
+    
+    //Option 2: Try NSPropertyListSerialization.
+    NSError *myError = nil;
+    NSData *xmlData = [NSPropertyListSerialization dataWithPropertyList:d format:NSPropertyListXMLFormat_v1_0 options:0 error:&myError];
+    
+    //NOTE: We do not yet know the address and port of the server.
+    //BUT: Suppose we have a harcoded servert/port.
+    NSString *urlString = @"http://demo.com/deviceToken";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = xmlData;
+    
+    
+    //Option 3: Just use the raw binary data.
+    request.HTTPBody = deviceToken;
+    //TODO: You may want to cash info about how often to do this.
+    //      You could keep a flag that says go to server.
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        //Check for errors.
+        NSLog(@"connectionError: %@", connectionError);
+        NSLog(@"response: %@", response);
+        if (response) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            NSLog(@"statusCode: %li", httpResponse.statusCode);
+        }
+    }];
+    
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    NSLog(@"Error: %@", error);
+}
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
